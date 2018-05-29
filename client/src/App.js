4ab534941ee37cb22355caac0b6af4c38001066c
Trey <treyw1850@gmail.com>
2018-05-29 12:16:47 -0500

    added a generic WORKING button

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Btn from './components/Buttons/GenericBtn/GenericBtn'
 
 class App extends Component {
   render() {
@@ -13,6 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Btn> I ams what I ams </Btn>
+
+
+
       </div>
     );
   }
