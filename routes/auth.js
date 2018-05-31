e0a81e23223096b5e295999974fc4c2ece763d69
Trey <treyw1850@gmail.com>
2018-05-31 17:56:41 -0500

    validation and sanitation

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+// let express = require('express');
+// let router = express.Router();
+
+// router.get('/', fuction(res, req, next) {
+//     res.render('index', {title: 'Form Validation', sucess: false, errors: req.session.errors});
+//     req.session.errors = null;
+// }); 
