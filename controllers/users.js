2ad6b2d43471d697733d8696ef5b0e24e072c148
Trey <treyw1850@gmail.com>
2018-06-04 06:52:58 -0400

    got everything working again

--- controllers/users.js
+++ controllers/users.js
@@ -2,12 +2,21 @@ const User = require('../models/Users');
 
 module.exports = {
     register: async (req, res, next) => {
-        // All user information (req.value.body)
-        console.log('UsersController.register() called !');
-
+        console.log('you hit me')
+        // deconstruct
         const {userName, firstName, lastName, email, password} = req.body;
+
+        // check for duplicate emails
+        const copy = await User.findOne({email});
+        if (copy) {
+            console.log('i founc a copy')
+            return res.status(403).json({error: 'There is already an account associated with this Email'})
+        }
+
+        // construct a new user and save to db
         const newUser = new User({ userName, firstName, lastName, email, password })
         await newUser.save();
+        console.log('im trying herer')
 
         res.json({ user: "created"});
     },
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 // const router = require('express-promise-router')();
 const UsersControllers = require('../../controllers/users');
-const { validateBody, schemas } = require('../../helpers/routeHelpers');
+const {validateBody, schemas} = require('../../helpers/routeHelpers');
 
 router.route('/register')
-    // .post(UsersControllers.register);
+    // .post(UsersControllers.register); //no-auth route for testing
     .post(validateBody(schemas.authSchema), UsersControllers.register);
 
 router.route('/sign-in')
