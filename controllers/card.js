68d0f3587f698cd00bf2ad759f05cadfd00400d3
Trey <treyw1850@gmail.com>
2018-06-08 11:14:14 -0400

    have 3 new controllers in two files

--- controllers/card.js
+++ controllers/card.js
@@ -2,17 +2,19 @@ const mongoose = require("mongoose");
 const db = require('../models');
 
 module.exports = {
+
+    // return all cards from the db
     findAll: async (req, res, next) => {
-    db.NoteCard.find({})
-    await (res => res.json()) 
-    .catch (res => res.json(error))
-},
+        db.NoteCard.find({})
+        await (res => res.json())
+            .catch(res => res.json(error))
+    },
 
-findAll: async (req, res, next) => {
-    db.NoteCard.find({})
-    await (res => res.json()) 
-    .catch (res => res.json(error))
-},
-
+    // return cards within a given category
+    findbyCat: async (req, res, next) => {
+        db.NoteCard.find(({category: req.body.cat}))
+        await (res => res.json())
+            .catch(res => res.json(error))
+    },
 
 }
